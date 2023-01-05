7f6909050bd456c0a90857b7ae8d03653ba702aa
igor almeida <igorm409@gmail.com>
2023-01-04 22:44:56 -0300

    inplementações necessarias realizadas com exito

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("99845588", "nokia a70", "4555554444", 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("telegram");
+
+Console.WriteLine("\n");
+
+Iphone iphone = new Iphone("5558488", "iphone 14", "11115544858", 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("instagram");
+iphone.ReceberLigacao();
